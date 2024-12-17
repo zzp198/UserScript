1d970368395bdde44d690bd2e547041956efd24c
Mr.Zhang <129676925+zzp198@users.noreply.github.com>
2024-12-17 20:22:27 +0800

    Update B站广告屏蔽.js

--- B站广告屏蔽.js
+++ B站广告屏蔽.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站广告屏蔽
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-16
+// @version      2024-12-14
 // @description  try to take over the world!
 // @author       You
 // @match        *://*.bilibili.com/*
